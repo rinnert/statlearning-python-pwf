228f2b773873ddf004e8bd2b57494775f388a1c1
Kurt Rinnert <kurtrinnert@cern.ch>
2019-08-22 23:39:08 +0200

    Add wacom config for RISE presentations.

---
+++ config/wacom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# pad
+xsetwacom set 17 Button 9 "key PgUp" 
+xsetwacom set 17 Button 8 "key PgDn"
+xsetwacom set 17 Button 1 "key ["
+xsetwacom set 17 Button 3 "key ]"
+
+# stylus
+xsetwacom set 15 Button 2 "key /"
+xsetwacom set 15 Button 3 "key ."
+
+# eraser
+xsetwacom set 18 Button 1 3
+xsetwacom set 18 Button 2 "key -"
+xsetwacom set 18 Button 3 "key ="
