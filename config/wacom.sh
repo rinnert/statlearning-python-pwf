ef9067be288c9bca0d52edf532919a8f5e4cce92
Kurt Rinnert <kurtrinnert@cern.ch>
2019-08-26 14:10:12 +0200

    Detect device id's.

--- config/wacom.sh
+++ config/wacom.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+pad=`xsetwacom list | grep "PAD" | gawk '{ id=NF-2; print($id);}'`
+stylus=`xsetwacom list | grep "STYLUS" | gawk '{ id=NF-2; print($id);}'`
+eraser=`xsetwacom list | grep "ERASER" | gawk '{ id=NF-2; print($id);}'`
+
 # pad
-xsetwacom set 17 Button 9 "key PgUp" 
-xsetwacom set 17 Button 8 "key PgDn"
-xsetwacom set 17 Button 1 "key ["
-xsetwacom set 17 Button 3 "key ]"
+xsetwacom set ${pad} Button 9 "key PgUp" 
+xsetwacom set ${pad} Button 8 "key PgDn"
+xsetwacom set ${pad} Button 1 "key ["
+xsetwacom set ${pad} Button 3 "key ]"
 
 # stylus
-xsetwacom set 15 Button 2 "key f"
-xsetwacom set 15 Button 3 "key g"
+xsetwacom set ${stylus} Button 2 "key f"
+xsetwacom set ${stylus} Button 3 "key g"
 
 # eraser
-xsetwacom set 18 Button 1 3
-xsetwacom set 18 Button 2 "key -"
-xsetwacom set 18 Button 3 "key ="
+xsetwacom set ${eraser} Button 1 3
+xsetwacom set ${eraser} Button 2 "key -"
+xsetwacom set ${eraser} Button 3 "key ="
