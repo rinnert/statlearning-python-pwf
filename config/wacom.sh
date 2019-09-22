347cc0dd54dee0c7ea2abc4d53277494981464cb
Kurt Rinnert <kurtrinnert@cern.ch>
2019-09-22 19:22:20 +0200

    Update wacom config script.

--- config/wacom.sh
+++ config/wacom.sh
@@ -3,9 +3,11 @@
 pad=`xsetwacom list | grep "PAD" | gawk '{ id=NF-2; print($id);}'`
 stylus=`xsetwacom list | grep "STYLUS" | gawk '{ id=NF-2; print($id);}'`
 eraser=`xsetwacom list | grep "ERASER" | gawk '{ id=NF-2; print($id);}'`
+touch=`xsetwacom list | grep "TOUCH" | gawk '{ id=NF-2; print($id);}'`
 
 # pad
 xsetwacom set ${pad} RawSample 12
+xsetwacom set ${pad} Suppress 10
 xsetwacom set ${pad} Button 9 "key PgUp" 
 xsetwacom set ${pad} Button 8 "key PgDn"
 xsetwacom set ${pad} Button 1 "key ["
@@ -13,11 +15,17 @@ xsetwacom set ${pad} Button 3 "key ]"
 
 # stylus
 xsetwacom set ${stylus} RawSample 12
+xsetwacom set ${stylus} Suppress 10
 xsetwacom set ${stylus} Button 2 "key f"
 xsetwacom set ${stylus} Button 3 "key g"
 
 # eraser
+xsetwacom set ${eraser} Suppress 10
 xsetwacom set ${eraser} RawSample 12
 xsetwacom set ${eraser} Button 1 3
 xsetwacom set ${eraser} Button 2 "key -"
 xsetwacom set ${eraser} Button 3 "key ="
+
+# touch
+xsetwacom set ${touch} Touch off
+xsetwacom set ${touch} Gesture off
