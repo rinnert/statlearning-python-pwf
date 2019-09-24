ad8c0d93966c0727bb48b66e7a1b3241c38a6a3e
Kurt Rinnert <kurtrinnert@cern.ch>
2019-09-25 01:57:04 +0200

    Add documetation for gene expression datasets.
    Note: the documentation does not currently match the format of the data
    in the islpy.h5 blob.  We need a more sophisticated R -> HDF script; to
    be addressed soon.

--- isl_python_package/islpy/datasets.py
+++ isl_python_package/islpy/datasets.py
@@ -439,7 +439,43 @@ def Khan(force_reload=False):
     """
     Return data frame for Khan dataset.
 
-    Gene expression measurements for four cancer types.
+    Description:
+    
+         The data consists of a number of tissue samples corresponding to
+         four distinct types of small round blue cell tumors. For each
+         tissue sample, 2308 gene expression measurements are available.
+    
+    Format:
+    
+         The format is a list containing four components: ‘xtrain’,
+         ‘xtest’, ‘ytrain’, and ‘ytest’. ‘xtrain’ contains the 2308 gene
+         expression values for 63 subjects and ‘ytrain’ records the
+         corresponding tumor type. ‘ytrain’ and ‘ytest’ contain the
+         corresponding testing sample information for a further 20
+         subjects.
+    
+    Source:
+    
+         This data were originally reported in:
+    
+         Khan J, Wei J, Ringner M, Saal L, Ladanyi M, Westermann F,
+         Berthold F, Schwab M, Antonescu C, Peterson C, and Meltzer P.
+         Classification and diagnostic prediction of cancers using gene
+         expression profiling and artificial neural networks. Nature
+         Medicine, v.7, pp.673-679, 2001.
+    
+         The data were also used in:
+    
+         Tibshirani RJ, Hastie T, Narasimhan B, and G. Chu. Diagnosis of
+         Multiple Cancer Types by Shrunken Centroids of Gene Expression.
+         Proceedings of the National Academy of Sciences of the United
+         States of America, v.99(10), pp.6567-6572, May 14, 2002.
+    
+    References:
+    
+         James, G., Witten, D., Hastie, T., and Tibshirani, R. (2013) _An
+         Introduction to Statistical Learning with applications in R_,
+         <URL: www.StatLearning.com>, Springer-Verlag, New York
     """
     global _khan_ds
     if force_reload or _khan_ds is None:
@@ -452,7 +488,29 @@ def NCI60(force_reload=False):
     """
     Return data frame for NCI60 dataset.
 
-    Gene expression measurements for 64 cancer types.
+    Description:
+    
+         NCI microarray data. The data contains expression levels on 6830
+         genes from 64 cancer cell lines. Cancer type is also recorded.
+    
+    Format:
+    
+         The format is a list containing two elements: ‘data’ and ‘labs’.
+    
+         ‘data’ is a 64 by 6830 matrix of the expression values while
+         ‘labs’ is a vector listing the cancer types for the 64 cell lines.
+    
+    Source:
+    
+         The data come from Ross et al. (Nat Genet., 2000). More
+         information can be obtained at
+         http://genome-www.stanford.edu/nci60/
+    
+    References:
+    
+         James, G., Witten, D., Hastie, T., and Tibshirani, R. (2013) _An
+         Introduction to Statistical Learning with applications in R_,
+         <URL: www.StatLearning.com>, Springer-Verlag, New York
     """
     global _nci60_ds
     if force_reload or _nci60_ds is None:
