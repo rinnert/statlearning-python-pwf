1f60e065e3389eb5a245dee551744ed99c6dd810
Kurt Rinnert <kurtrinnert@cern.ch>
2019-10-30 16:56:43 +0100

    Add category encoding.

--- isl_python_package/islpy/utils.py
+++ isl_python_package/islpy/utils.py
@@ -1,5 +1,8 @@
+import re
 import numpy as np
 import pandas as pd
+import patsy
+
 
 def marginalised_range(columns, data, points=100, others=None):
     """Return data frame with range in column column and 
@@ -20,3 +23,43 @@ def marginalised_range(columns, data, points=100, others=None):
             xs[name] = data[name]
 
     return xs
+
+
+def encode_categories(data, formula=None):
+    """Return new data frame with category encoding and flattened names."""
+
+    if formula is None:
+        formula = '+'.join(data.columns)
+
+    r_cat = re.compile(r'(?P<name>.+)\[(?P<trans>[TSDH]?)\.(?P<category>.+)\]') 
+    r_cexp = re.compile(r'C\((?P<name>.+), .+\)') 
+    r_poly = re.compile(r'C\((?P<name>.+), Poly\)\.(?P<category>.+)')
+
+    df = patsy.dmatrix(formula, data, return_type='dataframe')
+    
+    new_names = {}
+    for name in df.columns:
+        m_cat = r_cat.match(name)
+        m_poly = r_poly.match(name)
+        if m_cat:
+            new_name = m_cat['name']
+            m_cexp = r_cexp.match(new_name)
+            if m_cexp:
+                new_name = m_cexp['name']
+            category = m_cat['category']
+            trans = m_cat['trans']
+            if trans:
+                new_name += '_' + trans
+            new_name += '_' + category
+            new_names[name] = new_name
+        elif m_poly:
+            new_name = m_poly['name'] + '_' + m_poly['category']
+            new_names[name] = new_name
+
+    df.rename(new_names, axis=1, inplace=True)
+    try:
+        df.drop('Intercept', axis=1, inplace=True)
+    except KeyError:
+        pass
+
+    return df
