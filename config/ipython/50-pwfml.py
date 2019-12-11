baf361c3a57dda5a5fc84f1dbb0d3cbd03532159
Kurt Rinnert <kurtrinnert@cern.ch>
2019-12-11 18:02:05 +0100

    Fix python path in ipython configuration.

--- config/ipython/50-pwfml.py
+++ config/ipython/50-pwfml.py
@@ -1,4 +1,4 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.expanduser('~'), 'notebooks', 'utils'))
+sys.path.insert(0, os.path.join(os.path.expanduser('~'), 'notebooks', , 'pwfml', 'utils'))
 
